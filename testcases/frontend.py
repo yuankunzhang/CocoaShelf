6ed676c214ec0b17dc7f30501d3a5091164aca16
Vincent <feber007@gmail.com>
2013-04-04 15:45:30 +0800

    delete some pages.

--- testcases/frontend.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from testcases import TestCase
-
-class TestFrontend(TestCase):
-
-    def test_index(self):
-        response = self.client.get('/')
-        assert response.status_code == 200
